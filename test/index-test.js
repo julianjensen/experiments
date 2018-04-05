d9551b1e50122e602feb9c93fd392e9cff5b0001
Julian Jensen <jjdanois@gmail.com>
2018-04-05 07:56:48 -0400

    Initial commit

---
+++ test/index-test.js
@@ -0,0 +1,19 @@
+/** ******************************************************************************************************************
+ * @file Description of file here.
+ * @author Julian Jensen <jjdanois@gmail.com>
+ * @since 1.0.0
+ * @date Thu Apr 05 2018
+ *********************************************************************************************************************/
+"use strict";
+
+const
+    expect = require( 'chai' ).expect,
+    app = require( '../' );
+
+describe( 'experiments', function() {
+
+    it( 'should do stuff', () => {
+        expect( 1 ).to.equal( 1 );
+    } );
+
+} );
